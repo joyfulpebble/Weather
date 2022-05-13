c2ca14f10ef9c5cab7fbb8250e09b25b138de8d7
joyfulpebble <danmax32100@gmail.com>
2022-05-13 14:47:08 +0300

    Create canvas and canvas auto resizing

--- js/mainPageBackground.js
+++ js/mainPageBackground.js
@@ -1,1 +1,16 @@
-// go
+class Gradient {
+  constructor(){
+    this.cnv = document.querySelector('canvas');
+
+    this.setCanvasSize()
+    window.onresize = () => {
+      this.setCanvasSize()
+    }
+  }
+  setCanvasSize(){
+    this.w = this.cnv.width  = innerWidth;
+    this.h = this.cnv.height = innerHeight;
+  }
+}
+
+window.onload = () => new Gradient();
