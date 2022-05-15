c9ddfa5ada3acbaf6c02681978e8e7875cec5d8e
joyfulpebble <danmax32100@gmail.com>
2022-05-15 20:10:19 +0300

    Outline data display on the main page

--- src/gettingAForecast.js
+++ src/gettingAForecast.js
@@ -3,10 +3,39 @@ const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 const getGeoCode = async () => {
   const response = await fetch(locationURL)
   const locationData = await response.json()
-
+  
   getCoords(locationData)
 }
 getGeoCode()
+
+function drawForecast(data) {
+  const $container = document.querySelector('.app__container')
+  
+    const $app = 
+    `
+    <div class="app">
+      <div class="top">
+        <div class="temp">${data.current.temp_c}</div>
+        <div class="wind">
+          <div class="wind__dir"></div>
+          <div class="wind__speed"></div>
+        </div>
+      </div>
+      <div class="center">
+        <div class="icon"></div>
+        <div class="info"></div>
+      </div>
+      <div class="bottom">
+        <div class="fiilslike"></div>
+        <div class="humidity"></div>
+        <div class="precip"></div>
+      </div>
+    </div>
+    `
+    $container.insertAdjacentHTML('beforeend', $app)
+
+  console.log(data);
+}
 
 function getCoords(locationData) {
   let latitude = locationData.latitude
@@ -16,9 +45,8 @@ function getCoords(locationData) {
   const getForecast = async () => {
     const response = await fetch(weatherURL)
     const forecastData = await response.json()
-  
-    console.log(forecastData);
-    return forecastData;
+    // console.log(forecastData);
+    drawForecast(forecastData)
   }
   getForecast()
 }
