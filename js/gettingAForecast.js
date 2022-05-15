448563598835308ae35f0653f144cf0db217aefa
joyfulpebble <danmax32100@gmail.com>
2022-05-15 21:03:19 +0300

    Filling in the fields of the mini app

--- js/gettingAForecast.js
+++ js/gettingAForecast.js
@@ -8,31 +8,32 @@ const getGeoCode = async () => {
 }
 getGeoCode()
 
-function drawForecast(data) {
-  const $container = document.querySelector('.app__container')
-  
-    const $app = 
+function drawForecastOnMainPAge(data) {
+  const $appContainer = document.querySelector('.app__container')
+  var windToMps = data.current.wind_kph / 3.6;
+  var windDir = data.current.wind_dir.slice(0,-1);
+  const $app = 
     `
     <div class="app">
       <div class="top">
         <div class="temp">${data.current.temp_c}</div>
         <div class="wind">
-          <div class="wind__dir"></div>
-          <div class="wind__speed"></div>
+          <div class="wind__dir">Направление верта: ${windDir}</div>
+          <div class="wind__speed">Ветер м/с: ${windToMps.toFixed(2)}</div>
         </div>
       </div>
       <div class="center">
-        <div class="icon"></div>
-        <div class="info"></div>
+        <div class="icon"> <img src="${data.current.condition.icon}"> </div>
+        <div class="info">${data.current.condition.text}</div>
       </div>
       <div class="bottom">
-        <div class="fiilslike"></div>
-        <div class="humidity"></div>
-        <div class="precip"></div>
+        <div class="fiilslike">Ощущается как: ${data.current.feelslike_c}</div>
+        <div class="humidity">Влажность: ${data.current.humidity}</div>
+        <div class="precip">Осадки мм: ${data.current.precip_mm}</div>
       </div>
     </div>
     `
-    $container.insertAdjacentHTML('beforeend', $app)
+      $appContainer.insertAdjacentHTML('beforeend', $app)
 
   console.log(data);
 }
@@ -46,7 +47,7 @@ function getCoords(locationData) {
     const response = await fetch(weatherURL)
     const forecastData = await response.json()
     // console.log(forecastData);
-    drawForecast(forecastData)
+    drawForecastOnMainPAge(forecastData)
   }
   getForecast()
 }
