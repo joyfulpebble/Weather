fc80fcf329aa6c0a03a76d1f25371673e99248e8
joyfulpebble <danmax32100@gmail.com>
2022-05-19 19:49:30 +0300

    Completely changed data acquisition from API

---
+++ js/gettingForecast.js
@@ -0,0 +1,30 @@
+function weather() {
+  const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+  function getPosition(url) {
+    axios.get(locationURL).then(responce => {
+      let latitude  = responce.data.latitude;
+      let longitude = responce.data.longitude;
+  
+      getForecast(latitude, longitude)   
+    })
+  }
+  function getForecast(latitude, longitude) {
+    axios({
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/forecast.jsondays=3&aqi=no&alerts=no`,
+      params: {
+        key: '3d3037618cbf4db4a01120151221005',
+        q: `${latitude},${longitude}`,
+        days: '3',
+        aqi: 'no',
+        alerts: 'no',
+      }
+    })
+      .then(responce => {
+        
+        console.log(responce.data);
+      })
+  }
+  getPosition(locationURL)
+}
+weather()
