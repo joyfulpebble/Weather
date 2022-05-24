b20fcfe8d32c6d54eec79e2e150e3033eaa80659
joyfulpebble <danmax32100@gmail.com>
2022-05-24 21:04:23 +0300

    Sketched a change in the mode of obtaining a forecast, so far it works crookedly

--- js/getFullForecast.js
+++ js/getFullForecast.js
@@ -1,42 +1,51 @@
 const $SearchButton = document.querySelector('.search__btn');
+let current = true;
+
+if(current){
+  currentWeather()
+}
 
 $SearchButton.addEventListener('click',() => {
+  current = false;
+  if (!current) {
+    console.log('current clear');
+  }
   weather()
   // console.log('ok');
 })
 
-// function currentWeather() {
-//   const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
-//   function getCurrentPosition(url) {
-//     axios.get(url).then(responce => {
-  //       let latitude  = responce.data.latitude;
-//       let longitude = responce.data.longitude;
+
+function currentWeather() {
+  const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+  function getCurrentPosition(url) {
+    axios.get(url).then(responce => {
+        let latitude  = responce.data.latitude;
+      let longitude = responce.data.longitude;
   
-//       getForecast(latitude, longitude)   
-//     })
-//   }
-//   function getForecast(latitude, longitude) {
-//     axios({
-//       method: 'GET',
-//       url: `http://api.weatherapi.com/v1/forecast.json`,
-//       params: {
-//         key: '3d3037618cbf4db4a01120151221005',
-//         q: `${latitude},${longitude}`,
-//         days: '3',
-//         aqi: 'no',
-//         alerts: 'no',
-//       }
-//     })
-//       .then(responce => {
-//         const allForecast = responce.data;
+      getForecast(latitude, longitude)   
+    })
+  }
+  function getForecast(latitude, longitude) {
+    axios({
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/forecast.json`,
+      params: {
+        key: '3d3037618cbf4db4a01120151221005',
+        q: `${latitude},${longitude}`,
+        days: '3',
+        aqi: 'no',
+        alerts: 'no',
+      }
+    })
+      .then(responce => {
+        const allForecast = responce.data;
 
-//         console.log(allForecast);
-//       })
-//   }
-//   getCurrentPosition(locationURL)
-// }
+        console.log(allForecast);
+      })
+  }
+  getCurrentPosition(locationURL)
+}
 
-// currentWeather()
 
 function weather() {
   function getCurrentPosition() {
