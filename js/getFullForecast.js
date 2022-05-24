f70a135e8917dcd09bcd98cb5f8d609a7d7a749b
joyfulpebble <danmax32100@gmail.com>
2022-05-24 19:23:09 +0300

    Outline of getting a forecast by value from an input

---
+++ js/getFullForecast.js
@@ -0,0 +1,48 @@
+
+function weather() {
+  // const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+  function getCurrentPosition() {
+    var input = document.querySelector('.search__bar').value;
+    var inp = input
+    axios({
+      method: 'GET',
+      url: `http://autocomplete.travelpayouts.com/places2`,
+      params: {
+        term: inp,
+        locale: 'ru',
+        types: 'city'
+      }
+    }).then(responce => {
+      const allForecast = responce.data;
+      console.log(allForecast);
+      console.log(inp);
+    })
+    // axios.get(url).then(responce => {
+    //   let latitude  = responce.data.latitude;
+    //   let longitude = responce.data.longitude;
+  
+    //   getForecast(latitude, longitude)   
+    // })
+  }
+//   function getForecast(latitude, longitude) {
+//     axios({
+//       method: 'GET',
+//       url: `http://api.weatherapi.com/v1/forecast.json`,
+//       params: {
+//         key: '3d3037618cbf4db4a01120151221005',
+//         q: `${latitude},${longitude}`,
+//         days: '3',
+//         aqi: 'no',
+//         alerts: 'no',
+//       }
+//     })
+//       .then(responce => {
+//         const allForecast = responce.data;
+//           renderMiniApp(allForecast)
+
+//         // console.log(allForecast);
+//       })
+//   }
+  getCurrentPosition()
+}
+weather()
