ba78c907687351d4fc9d8734e8ddc008a7c310c8
joyfulpebble <danmax32100@gmail.com>
2022-05-24 20:46:44 +0300

    Getting forecast after click on button

--- js/getFullForecast.js
+++ js/getFullForecast.js
@@ -1,55 +1,18 @@
-function currentWeather() {
-  const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
-  function getCurrentPosition(url) {
-    axios.get(url).then(responce => {
-      let latitude  = responce.data.latitude;
-      let longitude = responce.data.longitude;
+const $SearchButton = document.querySelector('.search__btn');
+
+$SearchButton.addEventListener('click',() => {
+  weather()
+  // console.log('ok');
+})
+
+// function currentWeather() {
+//   const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+//   function getCurrentPosition(url) {
+//     axios.get(url).then(responce => {
+  //       let latitude  = responce.data.latitude;
+//       let longitude = responce.data.longitude;
   
-      getForecast(latitude, longitude)   
-    })
-  }
-  function getForecast(latitude, longitude) {
-    axios({
-      method: 'GET',
-      url: `http://api.weatherapi.com/v1/forecast.json`,
-      params: {
-        key: '3d3037618cbf4db4a01120151221005',
-        q: `${latitude},${longitude}`,
-        days: '3',
-        aqi: 'no',
-        alerts: 'no',
-      }
-    })
-      .then(responce => {
-        const allForecast = responce.data;
-
-        console.log(allForecast);
-      })
-  }
-  getCurrentPosition(locationURL)
-}
-
-currentWeather()
-
-// function weather() {
-//   function getCurrentPosition() {
-//     var input = document.querySelector('.search__bar').value;
-//     var inp = input
-//     axios({
-//       method: 'GET',
-//       url: `http://autocomplete.travelpayouts.com/places2`,
-//       params: {
-//         term: inp,
-//         locale: 'ru',
-//         types: 'city'
-//       }
-//     }).then(responce => {
-//       // const allForecast = responce.data;
-//       let latitude  = responce.data[0].coordinates.lat;
-//       let longitude = responce.data[0].coordinates.lon;
-//       getForecast(latitude, longitude)
-//       // console.log(allForecast);
-//       console.log(inp);
+//       getForecast(latitude, longitude)   
 //     })
 //   }
 //   function getForecast(latitude, longitude) {
@@ -66,10 +29,52 @@ currentWeather()
 //     })
 //       .then(responce => {
 //         const allForecast = responce.data;
+
 //         console.log(allForecast);
 //       })
 //   }
-//   getCurrentPosition()
+//   getCurrentPosition(locationURL)
 // }
 
-// weather()
+// currentWeather()
+
+function weather() {
+  function getCurrentPosition() {
+    var input = document.querySelector('.search__bar').value;
+    var inp = input
+    axios({
+      method: 'GET',
+      url: `http://autocomplete.travelpayouts.com/places2`,
+      params: {
+        term: inp,
+        locale: 'ru',
+        types: 'city'
+      }
+    }).then(responce => {
+      // const allForecast = responce.data;
+      let latitude  = responce.data[0].coordinates.lat;
+      let longitude = responce.data[0].coordinates.lon;
+      getForecast(latitude, longitude)
+      // console.log(allForecast);
+      console.log(inp);
+    })
+  }
+  function getForecast(latitude, longitude) {
+    axios({
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/forecast.json`,
+      params: {
+        key: '3d3037618cbf4db4a01120151221005',
+        q: `${latitude},${longitude}`,
+        days: '3',
+        aqi: 'no',
+        alerts: 'no',
+      }
+    })
+      .then(responce => {
+        const allForecast = responce.data;
+        console.log(allForecast);
+      })
+  }
+  getCurrentPosition()
+}
