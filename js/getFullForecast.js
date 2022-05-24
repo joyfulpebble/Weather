ac1113ccedcdf2367fe391334f100d33e709aedf
joyfulpebble <danmax32100@gmail.com>
2022-05-24 19:37:08 +0300

    It works but looks terrible

--- js/getFullForecast.js
+++ js/getFullForecast.js
@@ -1,6 +1,4 @@
-
 function weather() {
-  // const locationURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
   function getCurrentPosition() {
     var input = document.querySelector('.search__bar').value;
     var inp = input
@@ -13,36 +11,31 @@ function weather() {
         types: 'city'
       }
     }).then(responce => {
-      const allForecast = responce.data;
-      console.log(allForecast);
+      // const allForecast = responce.data;
+      let latitude  = responce.data[0].coordinates.lat;
+      let longitude = responce.data[0].coordinates.lon;
+      getForecast(latitude, longitude)
+      // console.log(allForecast);
       console.log(inp);
     })
-    // axios.get(url).then(responce => {
-    //   let latitude  = responce.data.latitude;
-    //   let longitude = responce.data.longitude;
-  
-    //   getForecast(latitude, longitude)   
-    // })
   }
-//   function getForecast(latitude, longitude) {
-//     axios({
-//       method: 'GET',
-//       url: `http://api.weatherapi.com/v1/forecast.json`,
-//       params: {
-//         key: '3d3037618cbf4db4a01120151221005',
-//         q: `${latitude},${longitude}`,
-//         days: '3',
-//         aqi: 'no',
-//         alerts: 'no',
-//       }
-//     })
-//       .then(responce => {
-//         const allForecast = responce.data;
-//           renderMiniApp(allForecast)
-
-//         // console.log(allForecast);
-//       })
-//   }
+  function getForecast(latitude, longitude) {
+    axios({
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/forecast.json`,
+      params: {
+        key: '3d3037618cbf4db4a01120151221005',
+        q: `${latitude},${longitude}`,
+        days: '3',
+        aqi: 'no',
+        alerts: 'no',
+      }
+    })
+      .then(responce => {
+        const allForecast = responce.data;
+        console.log(allForecast);
+      })
+  }
   getCurrentPosition()
 }
 weather()
